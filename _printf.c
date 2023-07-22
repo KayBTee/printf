873eff5134a1982a2bca8f4152570cdd8ddb9c8e
Leen <simplyleen2@gmail.com>
2023-07-22 08:18:10 -0700

    update

--- _printf.c
+++ _printf.c
@@ -17,8 +17,6 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0')
-				break;
 			if (*format != 'c' && *format != 's' && *format != '%')
 			{
 				count += write(1, "%", 1);
